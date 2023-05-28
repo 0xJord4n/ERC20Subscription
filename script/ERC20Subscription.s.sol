53faa6b10f199d52cc93e28a07d67fde50934cef
0xJord4n <me@0xjordan.dev>
2023-05-28 18:05:25 +0000

    first commit

---
+++ script/ERC20Subscription.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+
+contract ERC20SubscriptionScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+    }
+}
---
+++ test/ERC20Subscription.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/ERC20Subscription.sol";
+
+contract ERC20SubscriptionTest is Test {
+    
+}
