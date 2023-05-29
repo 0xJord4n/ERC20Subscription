750ceda2a52cf90945315b3db2d619299f3c814e
0xJord4n <me@0xjordan.dev>
2023-05-29 01:47:36 +0000

    Writed ERC20Subscription.sol and tests

--- src/ERC20Subscription.sol
+++ src/ERC20Subscription.sol
@@ -1,6 +1,140 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
-abstract contract ERC20Subscription {
-    
-}
+import "@opensezppling/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title ERC20Subscription
+ * @dev An extension of the ERC20 token standard that allows for a new type
+ of approval. This contract enables an owner to approve a spender to spend a
+ fixed amount of tokens on their behalf, with a custom recurrence interval
+ and for a specified duration of time. This allows for the creation
+ of subscriptions using ERC20 tokens.
+ */
+abstract contract ERC20Subscription is ERC20 {
+    mapping(address => mapping(address => mapping(uint32 => mapping(uint48 => uint256)))) private _allowances;
+    mapping(address => mapping(address => mapping(uint32 => mapping(uint48 => mapping(uint32 => uint256))))) private _spent;
+
+    /**
+     * @dev Approve an address to spend a certain amount of tokens on behalf of the owner for a subscription.
+     * @param spender The address to be approved.
+     * @param amount The amount of tokens to be approved.
+     * @param recurrenceInterval The interval at which the subscription recurs.
+     * @param approveUntil The time until which the approval is valid.
+     * @return A boolean value indicating whether the operation succeeded.
+     */
+    function approveForSubscription(
+        address spender,
+        uint256 amount,
+        uint32 recurrenceInterval,
+        uint48 approveUntil
+    ) external virtual returns (bool) {
+        _allowances[msg.sender][spender][recurrenceInterval][approveUntil] = amount;
+        return true;
+    }
+
+    /**
+     * @dev Get the allowance of a spender for a subscription on behalf of an owner.
+     * @param owner The address of the owner.
+     * @param spender The address of the spender.
+     * @param recurrenceInterval The interval at which the subscription recurs.
+     * @param approvedUntil The time until which the approval is valid.
+     * @return The allowance of the spender for a subscription on behalf of the owner.
+     */
+    function allowanceForSubscription(
+        address owner,
+        address spender,
+        uint32 recurrenceInterval,
+        uint48 approvedUntil
+    ) public view virtual returns (uint256) {
+        if (block.timestamp > approvedUntil && approvedUntil != 0) return 0;
+
+        uint32 period = uint32(block.timestamp) / recurrenceInterval;
+        uint256 allowance = _allowances[owner][spender][recurrenceInterval][approvedUntil];
+        uint256 spent = _spent[owner][spender][recurrenceInterval][approvedUntil][period];
+        return allowance - spent;
+    }
+
+    /**
+     * @dev Increase the allowance of a spender for a subscription on behalf of the owner.
+     * @param spender The address of the spender.
+     * @param addedAmount The amount of tokens to be added to the allowance.
+     * @param recurrenceInterval The interval at which the subscription recurs.
+     * @param approvedUntil The time until which the approval is valid.
+     * @return A boolean value indicating whether the operation succeeded.
+     */
+    function increaseAllowanceForSubscription(
+        address spender,
+        uint256 addedAmount,
+        uint32 recurrenceInterval,
+        uint48 approvedUntil
+    ) external virtual returns (bool) {
+        _allowances[msg.sender][spender][recurrenceInterval][approvedUntil] += addedAmount;
+        return true;
+    }
+
+    /**
+     * @dev Decrease the allowance of a spender for a subscription on behalf of the owner.
+     * @param spender The address of the spender.
+     * @param removedAmount The amount of tokens to be removed from the allowance.
+     * @param recurrenceInterval The interval at which the subscription recurs.
+     * @param approvedUntil The time until which the approval is valid.
+     * @return A boolean value indicating whether the operation succeeded.
+     */
+    function decreaseAllowanceForSubscription(
+        address spender,
+        uint256 removedAmount,
+        uint32 recurrenceInterval,
+        uint48 approvedUntil
+    ) external virtual returns (bool) {
+        _allowances[msg.sender][spender][recurrenceInterval][approvedUntil] -= removedAmount;
+        return true;
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another for a subscription.
+     * @param from The address to transfer from.
+     * @param to The address to transfer to.
+     * @param amount The amount of tokens to be transferred.
+     * @param recurrenceInterval The interval at which the subscription recurs.
+     * @param approvedUntil The time until which the approval is valid.
+     * @return A boolean value indicating whether the operation succeeded.
+     */
+    function transferFromForSubscription(
+        address from,
+        address to,
+        uint256 amount,
+        uint32 recurrenceInterval,
+        uint48 approvedUntil
+    ) external virtual returns (bool) {
+        _spendAllowance(from, to, amount, recurrenceInterval, approvedUntil);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    /**
+     * @dev Spend the allowance of a spender for a subscription on behalf of the owner.
+     * @param owner The address of the owner.
+     * @param spender The address of the spender.
+     * @param amount The amount of tokens to be spent.
+     * @param recurrenceInterval The interval at which the subscription recurs.
+     * @param approvedUntil The time until which the approval is valid.
+     */
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount,
+        uint32 recurrenceInterval,
+        uint48 approvedUntil
+    ) private {
+        uint256 currentAllowance = allowanceForSubscription(owner, spender, recurrenceInterval, approvedUntil);
+        require(amount <= currentAllowance, "ERC20Subscription: insufficient allowance");
+
+        uint256 fromBalance = balanceOf(owner);
+        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+
+        uint32 period = uint32(block.timestamp) / recurrenceInterval;
+
+        _spent[owner][spender][recurrenceInterval][approvedUntil][period] += amount;
+    }
+} 
--- test/ERC20Subscription.t.sol
+++ test/ERC20Subscription.t.sol
@@ -1,9 +1,210 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
+import "@opensezppling/contracts/token/ERC20/ERC20.sol";
 import "../src/ERC20Subscription.sol";
 
+contract TestToken is ERC20, ERC20Subscription {
+    constructor() ERC20("Test Token", "TEST") {
+        _mint(0x1814b7a2a132a816fF5Bd8573b1C2Bf5995d2FdA, 1000);
+    }
+}
+
 contract ERC20SubscriptionTest is Test {
+    TestToken public testToken;
+    address public bob = 0x1814b7a2a132a816fF5Bd8573b1C2Bf5995d2FdA;
+    uint48 public subscribeUntil;
+
+    function setUp() public {
+        testToken = new TestToken();
+        subscribeUntil = uint48(block.timestamp + 1 days);
+    }
+
+    function test_ApproveForSubscription() public {
+       testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+    }
+
+    function test_AllowanceForSubscription() public {
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+        uint256 allowance = testToken.allowanceForSubscription(address(this), address(this), 1 days, subscribeUntil);
+        
+        assertEq(allowance, 100);
+    }
+
+    function test_IncreaseAllowanceForSubscription() public {
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+        uint256 allowance = testToken.allowanceForSubscription(address(this), address(this), 1 days, subscribeUntil);
+        
+        assertEq(allowance, 100);
+        
+        testToken.increaseAllowanceForSubscription(address(this), 200, 1 days, subscribeUntil);
+        allowance = testToken.allowanceForSubscription(address(this), address(this), 1 days, subscribeUntil);
+        
+        assertEq(allowance, 300);
+    }
+
+    function test_RevertWhenOverflow_IncreaseAllowanceForSubscription() public {
+        testToken.approveForSubscription(address(this), type(uint256).max, 1 days, subscribeUntil);
+        uint256 allowance = testToken.allowanceForSubscription(address(this), address(this), 1 days, subscribeUntil);
+        
+        assertEq(allowance, type(uint256).max);
+        
+        vm.expectRevert(stdError.arithmeticError);
+
+        testToken.increaseAllowanceForSubscription(address(this), 1, 1 days, subscribeUntil);
+    }
+
+    function test_DecreaseAllowanceForSubscription() public {
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+        uint256 allowance = testToken.allowanceForSubscription(address(this), address(this), 1 days, subscribeUntil);
+
+        assertEq(allowance, 100);
+
+        testToken.decreaseAllowanceForSubscription(address(this), 50, 1 days, subscribeUntil);
+        allowance = testToken.allowanceForSubscription(address(this), address(this), 1 days, subscribeUntil);
+
+        assertEq(allowance, 50);
+    }
+
+    function test_RevertWhenUnderflow_DecreaseAllowanceForSubscription() public {
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+        
+        uint256 allowance = testToken.allowanceForSubscription(address(this), address(this), 1 days, subscribeUntil);
+
+        assertEq(allowance, 100);
+
+        vm.expectRevert(stdError.arithmeticError);
+
+        testToken.decreaseAllowanceForSubscription(address(this), 101, 1 days, subscribeUntil);
+    }
+
+    function test_TransferFromForSubscriptionWithApproval() public {
+        vm.startPrank(bob);
+        
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+
+        vm.stopPrank();
+
+        uint256 bobBalanceBefore = testToken.balanceOf(bob);
+        uint256 contractBalanceBefore = testToken.balanceOf(address(this));
+
+        testToken.transferFromForSubscription(bob, address(this), 100, 1 days, subscribeUntil);
+        
+        assertEq(testToken.balanceOf(bob), bobBalanceBefore - 100);
+        assertEq(testToken.balanceOf(address(this)), contractBalanceBefore + 100);
+    }
+
+    function test_TransferFromForSubscriptionWithApprovalUntilZero() public {
+        vm.startPrank(bob);
+        
+        testToken.approveForSubscription(address(this), 100, 1 days, 0);
+
+        vm.stopPrank();
+
+        for(uint i = 0; i < 10; i++) {
+            uint256 bobBalanceBefore = testToken.balanceOf(bob);
+            uint256 contractBalanceBefore = testToken.balanceOf(address(this));
+
+            testToken.transferFromForSubscription(bob, address(this), 100, 1 days, 0);
+
+            assertEq(testToken.balanceOf(bob), bobBalanceBefore - 100);
+            assertEq(testToken.balanceOf(address(this)), contractBalanceBefore + 100);
+
+            vm.warp(block.timestamp + 1 days);
+        }
+    }
+
+    function test_TransferFromForSubscriptionTwoDaysWithApproval() public {
+        vm.startPrank(bob);
+        
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil + 1 days);
+
+        vm.stopPrank();
+
+        for(uint i = 0; i < 2; i++) {
+            uint256 bobBalanceBefore = testToken.balanceOf(bob);
+            uint256 contractBalanceBefore = testToken.balanceOf(address(this));
+
+            testToken.transferFromForSubscription(bob, address(this), 100, 1 days, subscribeUntil + 1 days);
+
+            assertEq(testToken.balanceOf(bob), bobBalanceBefore - 100);
+            assertEq(testToken.balanceOf(address(this)), contractBalanceBefore + 100);
+
+            vm.warp(block.timestamp + 1 days);
+        }
+    }
+
     
+    function test_TransferFromForSubscriptionTwoTimesInSameIntervalWithApproval() public {
+        vm.startPrank(bob);
+        
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+
+        vm.stopPrank();
+
+        for(uint i = 0; i < 2; i++) {
+            uint256 bobBalanceBefore = testToken.balanceOf(bob);
+            uint256 contractBalanceBefore = testToken.balanceOf(address(this));
+
+            testToken.transferFromForSubscription(bob, address(this), 50, 1 days, subscribeUntil);
+
+            assertEq(testToken.balanceOf(bob), bobBalanceBefore - 50);
+            assertEq(testToken.balanceOf(address(this)), contractBalanceBefore + 50);
+        }
+    }
+
+    function test_RevertWhenAllowanceIsNotEnoughLong_TransferFromForSubscriptionTwoDaysButOneWithoutApproval() public {
+        vm.startPrank(bob);
+        
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+
+        vm.stopPrank();
+
+        for(uint i = 0; i < 2; i++) {
+            uint256 bobBalanceBefore = testToken.balanceOf(bob);
+            uint256 contractBalanceBefore = testToken.balanceOf(address(this));
+
+            if(i == 2) vm.expectRevert("ERC20Subscription: insufficient allowance");
+            testToken.transferFromForSubscription(bob, address(this), 100, 1 days, subscribeUntil);
+
+            assertEq(testToken.balanceOf(bob), bobBalanceBefore - 100);
+            assertEq(testToken.balanceOf(address(this)), contractBalanceBefore + 100);
+
+            vm.warp(block.timestamp + 1 days);
+        }
+    }
+
+    function test_RevertWhenNoAllowance_TransferFromForSubscriptionWithoutAllowance() public {
+        vm.expectRevert("ERC20Subscription: insufficient allowance");
+
+        testToken.transferFromForSubscription(bob, address(this), 100, 1 days, subscribeUntil);
+    }
+
+    function test_RevertWhenInsufficientBalance_TransferFromForSubscription() public {
+        vm.startPrank(bob);
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+
+        vm.stopPrank();
+
+        deal(address(testToken), bob, 0, true);
+
+        vm.expectRevert("ERC20: transfer amount exceeds balance");
+
+        testToken.transferFromForSubscription(bob, address(this), 100, 1 days, subscribeUntil);
+    }
+
+    function test_RevertWhenExpiredAllowance_TransferFromForSubscription() public {
+        vm.startPrank(bob);
+        
+        testToken.approveForSubscription(address(this), 100, 1 days, subscribeUntil);
+
+        vm.stopPrank();
+
+        vm.warp(block.timestamp + 2 days);
+
+        vm.expectRevert("ERC20Subscription: insufficient allowance");
+
+        testToken.transferFromForSubscription(bob, address(this), 100, 1 days, subscribeUntil);
+    }
 }
